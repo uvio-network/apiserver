394ea1f0083e9a91e55a43eccef4fd8ce7666c02
xh3b4sd <xh3b4sd@gmail.com>
2024-07-24 17:17:02 +0200

    update protobuf

--- pkg/server/serverhandler/posthandler/create.go
+++ pkg/server/serverhandler/posthandler/create.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/uvio-network/apigocode/pkg/post"
+	"github.com/uvio-network/apiserver/pkg/object/objectid"
 	"github.com/uvio-network/apiserver/pkg/server/context/userid"
 	"github.com/uvio-network/apiserver/pkg/server/converter"
 	"github.com/uvio-network/apiserver/pkg/storage/poststorage"
@@ -16,13 +17,15 @@ func (h *Handler) Create(ctx context.Context, req *post.CreateI) (*post.CreateO,
 	var inp []*poststorage.Object
 	for _, x := range req.Object {
 		inp = append(inp, &poststorage.Object{
-			Expiry: converter.StringToTime(x.Public.Expiry),
-			Kind:   x.Public.Kind,
-			Option: converter.StringToBool(x.Public.Option),
-			Owner:  userid.FromContext(ctx),
-			Stake:  converter.StringToFloat(x.Public.Stake),
-			Text:   x.Public.Text,
-			Token:  x.Public.Token,
+			Expiry:    converter.StringToTime(x.Public.Expiry),
+			Kind:      x.Public.Kind,
+			Lifecycle: x.Public.Lifecycle,
+			Option:    converter.StringToBool(x.Public.Option),
+			Owner:     userid.FromContext(ctx),
+			Parent:    objectid.ID(x.Public.Parent),
+			Stake:     converter.StringToFloat(x.Public.Stake),
+			Text:      x.Public.Text,
+			Token:     x.Public.Token,
 		})
 	}
 
@@ -51,7 +54,7 @@ func (h *Handler) Create(ctx context.Context, req *post.CreateI) (*post.CreateO,
 		res.Object = append(res.Object, &post.CreateO_Object{
 			Intern: &post.CreateO_Object_Intern{
 				Created: converter.TimeToString(x.Created),
-				Post:    x.ID.String(), // TODO rename to ID
+				Id:      x.ID.String(),
 			},
 		})
 	}
--- pkg/storage/poststorage/object.go
+++ pkg/storage/poststorage/object.go
@@ -9,18 +9,74 @@ import (
 )
 
 type Object struct {
-	Created time.Time   `json:"created"`
-	Expiry  time.Time   `json:"expiry"` // TODO expiry must be greater than created
-	ID      objectid.ID `json:"id"`
-	Kind    string      `json:"kind"` // TODO kind is either claim or comment
-	Option  bool        `json:"option"`
-	Owner   objectid.ID `json:"owner"` // TODO verify post creator once auth is setup
-	Stake   float64     `json:"stake"` // TODO stake must not be empty
-	Text    string      `json:"text"`
-	Token   string      `json:"token"`
+	Created   time.Time   `json:"created"`
+	Expiry    time.Time   `json:"expiry"`
+	ID        objectid.ID `json:"id"`
+	Kind      string      `json:"kind"`
+	Lifecycle string      `json:"lifecycle"`
+	Option    bool        `json:"option"`
+	Owner     objectid.ID `json:"owner"`  // TODO verify post creator once auth is setup
+	Parent    objectid.ID `json:"parent"` // TODO must exist if given
+	Stake     float64     `json:"stake"`
+	Text      string      `json:"text"`
+	Token     string      `json:"token"`
+	Tree      objectid.ID `json:"tree"`
 }
 
 func (o *Object) Verify() error {
+	{
+		if o.Expiry.IsZero() {
+			return tracer.Mask(PostExpiryEmptyError)
+		}
+
+		// When creating posts of kind "claim", we want to ensure that claims cannot
+		// be created with expiries that point to the past. During claim creation,
+		// the post IDs are only allocated once the input data got verified. During
+		// claim updates, the post expiry may very well be in the past at the time
+		// of the update being processed. During those update processes we do not
+		// want to run the check below again, assuming nobody from the outside could
+		// change the initial expiry anymore.
+		if o.ID == "" && o.Kind == "claim" && o.Expiry.Compare(time.Now().UTC()) != +1 {
+			return tracer.Mask(PostExpiryPastError)
+		}
+	}
+
+	{
+		if o.Kind != "claim" && o.Kind != "comment" {
+			return tracer.Maskf(PostKindInvalidError, o.Kind)
+		}
+	}
+
+	{
+		if o.Kind == "claim" && o.Lifecycle != "propose" && o.Lifecycle != "resolve" && o.Lifecycle != "dispute" && o.Lifecycle != "nullify" {
+			return tracer.Maskf(ClaimLifecycleInvalidError, o.Lifecycle)
+		}
+		if o.Kind == "comment" && o.Lifecycle != "" {
+			return tracer.Maskf(CommentLifecycleInvalidError, o.Lifecycle)
+		}
+	}
+
+	{
+		// Any claim with lifecycle other than "propose" must reference a parent.
+		if o.Kind == "claim" && o.Lifecycle != "propose" && o.Parent == "" {
+			return tracer.Maskf(ClaimParentEmptyError, o.Lifecycle)
+		}
+		// Any claim with lifecycle "propose" must not reference a parent.
+		if o.Kind == "claim" && o.Lifecycle == "propose" && o.Parent != "" {
+			return tracer.Maskf(ClaimParentInvalidError, o.Lifecycle)
+		}
+		// Any comment must reference its parent claim.
+		if o.Kind == "comment" && o.Parent == "" {
+			return tracer.Maskf(CommentParentEmptyError, o.Lifecycle)
+		}
+	}
+
+	{
+		if o.Stake <= 0 {
+			return tracer.Mask(PostStakeInvalidError)
+		}
+	}
+
 	{
 		txt := strings.TrimSpace(o.Text)
 
