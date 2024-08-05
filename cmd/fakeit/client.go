fc49935bd9f8dbdd51fdf0c7d84e65fe44da3321
xh3b4sd <xh3b4sd@gmail.com>
2024-08-05 20:27:16 +0200

    generate fake claims

--- cmd/fakeit/client.go
+++ cmd/fakeit/client.go
@@ -6,20 +6,36 @@ import (
 	"net/http"
 
 	"github.com/twitchtv/twirp"
+	"github.com/uvio-network/apigocode/pkg/post"
 	"github.com/uvio-network/apigocode/pkg/user"
 	"github.com/uvio-network/apiserver/pkg/envvar"
 )
 
 type Client struct {
+	Post post.API
 	User user.API
 }
 
 func NewClient(env envvar.Env) Client {
-	return Client{
-		User: user.NewAPIJSONClient(
-			fmt.Sprintf("http://%s", net.JoinHostPort(env.HttpHost, env.HttpPort)),
-			&http.Client{},
+	var url string
+	{
+		url = fmt.Sprintf("http://%s", net.JoinHostPort(env.HttpHost, env.HttpPort))
+	}
+
+	var cli *http.Client
+	{
+		cli = &http.Client{}
+	}
+
+	var opt []twirp.ClientOption
+	{
+		opt = []twirp.ClientOption{
 			twirp.WithClientPathPrefix(""),
-		),
+		}
+	}
+
+	return Client{
+		Post: post.NewAPIJSONClient(url, cli, opt...),
+		User: user.NewAPIJSONClient(url, cli, opt...),
 	}
 }
