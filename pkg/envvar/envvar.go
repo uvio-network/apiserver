14f019b05ad2ac3826e11e3be4c2770a198b6ddd
johnnyonline <johnnyonline.eth@gmail.com>
2024-08-20 16:09:40 +0300

    fix: https://github.com/uvio-network/apiserver/pull/49#discussion_r1721050993

--- pkg/envvar/envvar.go
+++ pkg/envvar/envvar.go
@@ -21,8 +21,8 @@ type Env struct {
 	ChainRpcEndpoint  string `split_words:"true" required:"true"`
 	HttpHost          string `split_words:"true" default:"127.0.0.1"`
 	HttpPort          string `split_words:"true" default:"7777"`
-	MarketsAddress    string `split_words:"true" default:"0xBa230f4Bf34E48D04e65dE9a0F6Fe5EcDAa0c17A"`
-	RandomizerAddress string `split_words:"true" default:"0x63f01b695c67B764e823F972bc61fcAFbac5102b"`
+	MarketsAddress    string `split_words:"true" required:"true"`
+	RandomizerAddress string `split_words:"true" required:"true"`
 }
 
 func Load(pat string) Env {
