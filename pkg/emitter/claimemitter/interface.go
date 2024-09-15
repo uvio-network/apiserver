8a3fc8ed95ad0f3d1a65375d16ec62226fb2bf11
xh3b4sd <xh3b4sd@gmail.com>
2024-09-15 12:02:24 +0200

    fix

--- pkg/emitter/claimemitter/interface.go
+++ pkg/emitter/claimemitter/interface.go
@@ -6,5 +6,11 @@ import (
 )
 
 type Interface interface {
+	// Create emits an event that intends to trigger the creation of a claim with
+	// the given lifecycle phase.
+	//
+	//     inp[0] the latest claim ID within a tree from which to create the next claim
+	//     inp[1] the desired lifecycle phase of the claim to create
+	//
 	Create(objectid.ID, objectlabel.DesiredLifecycle) error
 }
